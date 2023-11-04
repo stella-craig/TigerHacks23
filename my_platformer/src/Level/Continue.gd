4c10f1ac37c2fca896dc37abfaf60c03c7adcc3c
mbowman21 <sstraberry101@gmail.com>
2023-11-04 16:07:23 -0500

    work on going to next level

---
+++ my_platformer/src/Level/Continue.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Continue_button_down():
+	get_tree().change_scene("res://src/Level2/Level2.tscn")
--- my_platformer/src/Level/Door.gd
+++ my_platformer/src/Level/Door.gd
@@ -7,8 +7,8 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		if get_overlapping_bodies().size() == 1:
-# warning-ignore:return_value_discarded
-			get_tree().change_scene("res://src/MainMenu/MainMenu.tscn")
+			# warning-ignore:return_value_discarded
+			get_tree().change_scene("res://src/Level/Completed.tscn")
 
 
 func _on_Door_body_entered(_body):
