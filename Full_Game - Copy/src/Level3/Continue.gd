d672480047a1307ea8ec2b93196a1b71266db3b4
mbowman21 <sstraberry101@gmail.com>
2023-11-04 23:02:22 -0500

    commiting for now

--- Full_Game - Copy/src/Level3/Continue.gd
+++ Full_Game - Copy/src/Level3/Continue.gd
@@ -4,4 +4,4 @@ extends Button
 
 func _on_Continue_button_down():
 # warning-ignore:return_value_discarded
-	get_tree().change_scene("res://src/MainMenu/MainMenu.tscn")
+	get_tree().change_scene("res://src/Level4/Game.tscn")
