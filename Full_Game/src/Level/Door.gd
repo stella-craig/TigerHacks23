98f61c1c094408c886b84b183310efdf2541d560
mbowman21 <sstraberry101@gmail.com>
2023-11-04 19:48:18 -0500

    level3 works! now onto background

--- Full_Game/src/Level/Door.gd
+++ Full_Game/src/Level/Door.gd
@@ -8,7 +8,7 @@ func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		if get_overlapping_bodies().size() == 1:
 			# warning-ignore:return_value_discarded
-			get_tree().change_scene("res://src/Level/Completed.tscn")
+			get_tree().change_scene("res://src/Level/Completed1.tscn")
 
 
 func _on_Door_body_entered(_body):
---
+++ Full_Game/src/Level3/Door.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		if get_overlapping_bodies().size() == 1:
+			# warning-ignore:return_value_discarded
+			get_tree().change_scene("res://src/MainMenu/MainMenu.tscn")
