17951a06515a9769771b2842eaf9cbaa0fc94c33
mbowman21 <sstraberry101@gmail.com>
2023-11-04 21:16:43 -0500

    push up

--- Full_Game/src/DeathMessage/DeathMessage.gd
+++ Full_Game/src/DeathMessage/DeathMessage.gd
@@ -14,6 +14,18 @@ func _ready():
 func _on_RestartButton_button_down():
 # warning-ignore:unsafe_property_access
 	CoinsCounter.coins_collected = 0
+# warning-ignore:unsafe_property_access
+	CoinsCounter.level1_coins = 0
+# warning-ignore:unsafe_property_access
+	CoinsCounter.level2_coins = 0
+# warning-ignore:unsafe_property_access
+	CoinsCounter.level3_coins = 0
+# warning-ignore:unsafe_property_access
+	CoinsCounter.first_level = true
+# warning-ignore:unsafe_property_access
+	CoinsCounter.second_level = false
+# warning-ignore:unsafe_property_access
+	CoinsCounter.third_level = false
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://src/Main/Game.tscn")
 
