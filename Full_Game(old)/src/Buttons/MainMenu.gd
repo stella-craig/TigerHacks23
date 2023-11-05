7a3f5242bc6aa8d3f9061d4b15ce4934d65203f3
Stella <stellarstellacraig@gmail.com>
2023-11-04 20:17:55 -0500

    yes

---
+++ Full_Game(old)/src/Buttons/MainMenu.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_MainMenu_button_down():
+	get_tree().change_scene("res://src/MainMenu/MainMenu.tscn")
---
+++ Full_Game(old)/src/Level/Continue.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Continue_button_down():
+	get_tree().change_scene("res://src/Level3/Game.tscn")
